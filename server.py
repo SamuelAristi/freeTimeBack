80fd602b8d8b6559695fc7aa9945d227d94e8c23
JuanDaviid01 <127440006+JuanDaviid01@users.noreply.github.com>
2024-09-26 21:52:03 -0500

    Controller, Model y Router de tarea

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from Routers.cityRouter import city_router  # Importa un conjunto de rutas (router) desde otro archivo
-
+from Routers.taskRouter import task_router
 app = Flask(__name__)  # Crea una instancia de la aplicación Flask
 
 # Registrar el blueprint de city
 app.register_blueprint(city_router)  # Añade el conjunto de rutas al servidor Flask
-
+app.register_blueprint(task_router) 
 
 
 app.run(host='127.0.0.1', port=1337, debug=True)
