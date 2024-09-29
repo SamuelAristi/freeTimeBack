c0b6625e7aff2787c5e7b9b39861b48855519753
Luis Miguel Aguirre Serna <miguiluis90@gmail.com>
2024-09-28 23:31:58 -0500

    habilitado frontend con CORS

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from Routers.cityRouter import city_router  
 from Routers.supportRouter import support_router
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Direccion del frontend
+CORS(app, origins=["http://localhost:4200"])
 
 # Registrar los blueprints
 app.register_blueprint(city_router)
